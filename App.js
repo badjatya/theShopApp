b307d8bf4bf02322f393a803142c5d59286e7d24
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-04 22:24:52 +0530

    App Update

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from "react";
 
-// TODO: Importing Navigation
+// TODO Importing Navigation
 import Navigation from "./App/navigation/productsNavigator";
 
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
-// TODO: importing Reducers
+// TODO importing Reducers
 import productReducer from "./App/store/reducers/product.reducer";
 
-//TODO: Combining reducer and creating redux store
+// TODO Combining reducer and creating redux store
 const rootReducer = combineReducers({
   products: productReducer,
 });
 const store = createStore(rootReducer);
 
-//TODO: Main App Screen
+// TODO Main App Screen
 export default function App() {
   return (
     <Provider store={store}>
