af5d38064ab2830a6589944a39124ad5b78c5dd5
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-04 22:07:02 +0530

    ProductDetailScreen Content

--- App/components/shop/ProductItem/ProductItem.js
+++ App/components/shop/ProductItem/ProductItem.js
@@ -29,7 +29,7 @@ const ProductItem = (props) => {
             </View>
             <View style={styles.detail}>
               <Text style={styles.title}>{props.title}</Text>
-              <Text style={styles.price}>₹ {props.price}</Text>
+              <Text style={styles.price}>₹ {props.price.toFixed(2)}</Text>
             </View>
             <View style={styles.buttonContainer}>
               <Button title="View Detail" onPress={props.onViewDetail} />
--- App/screens/shop/ProductDetail/ProductDetailScreen.js
+++ App/screens/shop/ProductDetail/ProductDetailScreen.js
@@ -1,15 +1,48 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Button,
+  StyleSheet,
+} from "react-native";
+
+import { useSelector } from "react-redux";
 
 const ProductDetailScreen = (props) => {
+  // TODO => Getting Params from Navigation
   const { navigation, route } = props;
   const product = route.params;
 
+  // TODO => Selecting Product
+  const selectedProduct = useSelector((state) =>
+    state.products.availableProducts.find((pro) => pro.id === product.productId)
+  );
+
   return (
-    <View>
-      <Text>{product.productTitle}</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={{ uri: selectedProduct.imageUrl }}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Add to Cart" onPress={() => alert("Added to Cart")} />
+      </View>
+      <Text style={styles.price}>₹ {selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  imageContainer: {},
+  image: {},
+  buttonContainer: {},
+  price: {},
+  description: {},
+});
 
 export default ProductDetailScreen;
