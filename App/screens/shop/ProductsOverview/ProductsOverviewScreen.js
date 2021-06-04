c42d0ee04284290118d1e2a7eff3a4841b0d1218
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-04 15:44:10 +0530

    ProductsOverviewScreen

--- App/screens/shop/ProductsOverview/ProductsOverviewScreen.js
+++ App/screens/shop/ProductsOverview/ProductsOverviewScreen.js
@@ -4,12 +4,15 @@ import { View, FlatList, Text, StatusBar } from "react-native";
 // TODO: Importing Reducer
 import { useSelector } from "react-redux";
 
+//TODO: Importing Colors
+import Colors from "../../../constants/Colors";
+
 // TODO: Main Screen i.e ProductsOverviewScreen
 const ProductsOverviewScreen = () => {
   const products = useSelector((state) => state.products.availableProducts);
   return (
     <View>
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="light-content" backgroundColor={Colors.primary} />
       <FlatList
         data={products}
         renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
