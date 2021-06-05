b70b9a437068a54d2915885f1729d0d8d4c298c4
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-05 22:22:00 +0530

    Order Reducer created

---
+++ App/store/reducers/order.reducer.js
@@ -0,0 +1,11 @@
+import { ADD_ORDER } from "../actions/order.action";
+
+const initialState = {
+  order: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ORDER:
+  }
+};
