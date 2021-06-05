e761efa1d498251dc310b3a52c28bc6b2f5a1094
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-05 22:31:24 +0530

    Order reducer added

--- App/store/reducers/order.reducer.js
+++ App/store/reducers/order.reducer.js
@@ -1,4 +1,5 @@
 import { ADD_ORDER } from "../actions/order.action";
+import Order from "../../models/Order";
 
 const initialState = {
   order: [],
@@ -7,5 +8,16 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_ORDER:
+      const newOrder = new Order(
+        new Date().toString(),
+        action.orderData.items,
+        action.orderData.amount,
+        new Date()
+      );
+
+      return {
+        ...state,
+        orders: state.orders.concat(newOrder),
+      };
   }
 };
