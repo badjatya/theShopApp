d8e8ff65ed24749a26ffbf3a06f3c37bb3c0c559
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-06 20:34:26 +0530

    UserProductScreen designed

---
+++ App/screens/users/UserProducts/UserProductsScreen.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+// TODO: Importing redux store
+import { useSelector } from "react-redux";
+
+// TODO: Importing Component
+import ProductItem from "../../../components/shop/ProductItem";
+
+const UserProductsScreen = () => {
+  const userProducts = useSelector((state) => state.products.userProducts);
+
+  return (
+    <View>
+      <FlatList
+        data={userProducts}
+        renderItem={(itemData) => (
+          <ProductItem
+            imageUrl={itemData.item.imageUrl}
+            title={itemData.item.title}
+            price={itemData.item.price}
+            onViewDetail={() => alert("View Detail")}
+            onAddToCart={() => alert("Item Added")}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+export default UserProductsScreen;
+
+const styles = StyleSheet.create({});
