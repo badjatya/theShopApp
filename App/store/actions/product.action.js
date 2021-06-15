74732aefd89a92bef76a608a5a3557b61187de8f
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-15 23:40:10 +0530

    updating and deleting products on server

--- App/store/actions/product.action.js
+++ App/store/actions/product.action.js
@@ -70,18 +70,45 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl, price) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price,
-    },
+  return async (dispatch) => {
+    await fetch(
+      `https://theshopapp-24f57-default-rtdb.asia-southeast1.firebasedatabase.app/products/${id}.json`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price,
+        }),
+      }
+    );
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+        price,
+      },
+    });
   };
 };
 
 export const deleteProduct = (productId) => {
-  return { type: DELETE_PRODUCT, pid: productId };
+  return async (dispatch) => {
+    await fetch(
+      `https://theshopapp-24f57-default-rtdb.asia-southeast1.firebasedatabase.app/products/${productId}.json`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    dispatch({ type: DELETE_PRODUCT, pid: productId });
+  };
 };
