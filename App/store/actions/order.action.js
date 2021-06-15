03c6bb49c4ec765730af1edf4b48a7af306edc7c
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-16 00:25:40 +0530

    Storing orders on server

--- App/store/actions/order.action.js
+++ App/store/actions/order.action.js
@@ -1,8 +1,35 @@
 export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount },
+  return async (dispatch) => {
+    const date = new Date();
+
+    // Posting orders to server
+    const response = await fetch(
+      "https://theshopapp-24f57-default-rtdb.asia-southeast1.firebasedatabase.app/orders/u1.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString(),
+        }),
+      }
+    );
+
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date,
+      },
+    });
   };
 };
