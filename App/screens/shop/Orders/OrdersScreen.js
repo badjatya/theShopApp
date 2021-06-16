957e125eafde0768a89bd5fdeaf2e6ac20aa0827
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-16 20:58:05 +0530

    fetching orders from server

--- App/screens/shop/Orders/OrdersScreen.js
+++ App/screens/shop/Orders/OrdersScreen.js
@@ -1,18 +1,47 @@
-import React from "react";
-import { FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, View, ActivityIndicator, StyleSheet } from "react-native";
 
 // TODO: Importing redux store
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import * as ordersActions from "../../../store/actions/order.action";
 
 // TODO: Importing Components
-
 import OrderItem from "../../../components/shop/OrderItem/OrderItem";
 
 // TODO: Importing Colors
-// import Colors from "../../../constants/Colors";
+import Colors from "../../../constants/Colors";
 
 const OrdersScreen = () => {
+  //State
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Fetching orders
   const orders = useSelector((state) => state.orders.orders);
+
+  // Dispatch
+  const dispatch = useDispatch();
+
+  // Fetch order function
+  const fetchOrder = async () => {
+    setIsLoading(true);
+    dispatch(ordersActions.fetchOrders());
+    setIsLoading(false);
+  };
+
+  // Fetching orders from server
+  useEffect(() => {
+    fetchOrder();
+  }, [dispatch, fetchOrder]);
+
+  // For loading spinner
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={orders}
@@ -28,3 +57,11 @@ const OrdersScreen = () => {
 };
 
 export default OrdersScreen;
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
