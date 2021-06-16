bc870d45bcada29cf1f2284a410f5c8d57ee435c
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-16 20:43:18 +0530

    order action fetching orders from server

--- App/store/actions/order.action.js
+++ App/store/actions/order.action.js
@@ -1,4 +1,35 @@
+// TODO: Importing Order Model
+import Order from "../../models/orders.model";
+
 export const ADD_ORDER = "ADD_ORDER";
+export const SET_ORDER = "SET_ORDER";
+
+export const fetchOrders = () => {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://theshopapp-24f57-default-rtdb.asia-southeast1.firebasedatabase.app/orders/u1.json"
+    );
+
+    const resData = await response.json();
+    const loadedOrders = [];
+
+    for (const key in resData) {
+      loadedOrders.push(
+        new Order(
+          key,
+          resData[key].cartItems,
+          resData[key].totalAmount,
+          new Date(resData[key].date)
+        )
+      );
+    }
+
+    dispatch({
+      type: SET_ORDER,
+      orders: loadedOrders,
+    });
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async (dispatch) => {
