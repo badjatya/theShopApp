98e592169bec36244b7428e969f7e00f04648235
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-06-16 20:45:40 +0530

    orders fetching from server

--- App/store/reducers/order.reducer.js
+++ App/store/reducers/order.reducer.js
@@ -1,5 +1,5 @@
 // TODO: Importing Actions
-import { ADD_ORDER } from "../actions/order.action";
+import { ADD_ORDER, SET_ORDER } from "../actions/order.action";
 
 // TODO: Importing model Order
 import Order from "../../models/orders.model";
@@ -10,6 +10,12 @@ const initialState = {
 
 const orderReducer = (state = initialState, action) => {
   switch (action.type) {
+    //for fetching orders from server
+    case SET_ORDER:
+      return {
+        orders: action.orders,
+      };
+
     case ADD_ORDER:
       const newOrder = new Order(
         action.orderData.id,
